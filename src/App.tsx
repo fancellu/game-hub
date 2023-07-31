8e51a0940fcc29322560ab5bf66b66be5d1a284a
fancellu <dino@felstar.com>
2023-07-31 10:22:45 +0100

    Responsive layout for mobile and desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 
-import {Button} from '@chakra-ui/react'
+
 // import './App.css'
+
+import {Grid, GridItem, Show} from "@chakra-ui/react";
 
 function App() {
 
-  return (
-
-        <Button colorScheme='blue'>Button</Button>
-
-
-  )
+  return <Grid templateAreas={{
+    base: `"nav" "main"`,
+    lg: `"nav nav" "aside main"`
+  }}>
+    <GridItem bg='coral' area='nav'>Nav</GridItem>
+    <Show above="lg">
+      <GridItem bg='gold' area='aside'>Aside</GridItem>
+    </Show>
+    <GridItem bg='dodgerblue' area='main'>Main</GridItem>
+  </Grid>
 }
 
 export default App
