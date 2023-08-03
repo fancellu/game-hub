9f0947011cdae691b67ccf642a4b22051cecc947
fancellu <dino@felstar.com>
2023-08-03 12:46:14 +0100

    Cleanup game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({gameQuery}: Props) => {
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} spacing={4} padding="10px">
+            <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={6} padding="10px">
                 {isLoading &&
                     skeletons.map((skeleton) => (
                         <GameCardContainer key={skeleton}>
